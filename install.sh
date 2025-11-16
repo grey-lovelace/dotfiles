232708880bc1764b38ba4ebfb8bb73b50d8a2a33
Grey Lovelace <austin.grey.lovelace@gmail.com>
2025-11-16 14:48:51 -0600

    Try to use oh my posh

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 ln -s "$DOTFILES_DIR/.bash_aliases" "$HOME/.bash_aliases"
+
+# Install Oh My Posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
+echo 'eval "$(oh-my-posh init bash)"' >> $HOME/.bashrc
